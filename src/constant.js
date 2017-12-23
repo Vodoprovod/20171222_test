3396160e875910d136a89a60e162700d923866a4
Vodoprovod <naiznanku45@mail.ru>
2017-12-23 20:22:14 +0300

    src-public

---
+++ src/constant.js
@@ -0,0 +1,4 @@
+/**
+ * Created by TOY-2 on 23.12.2017.
+ */
+module.exports  = 'App init'
---
+++ src/index.js
@@ -0,0 +1,9 @@
+var $ = require('jquery');
+var str = require('./constant.js');
+
+var App = function () {
+    console.log(str);
+    $('body').html(str);
+}
+
+var app = new App();
